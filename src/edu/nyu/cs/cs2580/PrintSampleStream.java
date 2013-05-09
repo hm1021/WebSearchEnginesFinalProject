bf86f16a8ea2cb83c9b67cbcac808194ffd693c9
Hiral <hm1021@nyu.edu>
2013-05-09 19:31:52 -0400

    Testing

---
+++ src/edu/nyu/cs/cs2580/PrintSampleStream.java
@@ -0,0 +1,13 @@
+package edu.nyu.cs.cs2580;
+
+public class PrintSampleStream {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+	}
+
+}
